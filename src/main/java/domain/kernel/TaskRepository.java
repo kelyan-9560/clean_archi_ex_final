0bd75b7580470345b54f7e639e350dc02d9b038a
ahmad <ahmad@ahmads-MacBook-Air.local>
2023-02-18 18:35:33 +0100

    save json file added

--- src/main/java/domain/kernel/TaskRepository.java
+++ src/main/java/domain/kernel/TaskRepository.java
@@ -2,11 +2,12 @@ package domain.kernel;
 
 import domain.models.Task;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface TaskRepository {
-    void add(Task task);
+    void add(Task task) throws IOException;
     List<Task> list();
-    void remove(String id);
+    void remove(String id) throws IOException;
     Task getTaskById(String id);
 }
--- src/main/java/infrastructure/InFileRepository.java
+++ src/main/java/infrastructure/InFileRepository.java
@@ -1,19 +1,35 @@
 package infrastructure;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import domain.kernel.TaskRepository;
 import domain.models.Task;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 class InFileRepository implements TaskRepository {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final TypeReference<List<Task>> mapType = new TypeReference<List<Task>>() {};
+
+    InFileRepository(){
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
 
     private static final String jsonFilePath = "data.json";
     private List<Task> db = new ArrayList<Task>();
 
     @Override
-    public void add(Task task) {
+    public void add(Task task) throws IOException {
         this.db.add(task);
+        this.save();
+
     }
 
     @Override
@@ -22,13 +38,14 @@ class InFileRepository implements TaskRepository {
     }
 
     @Override
-    public void remove(String id) {
+    public void remove(String id) throws IOException {
         for(Task task: this.db){
             if(task.getId().getId().equals(id)){
                 this.db.remove(task);
                 return;
             }
         }
+        this.save();
     }
     @Override
     public Task getTaskById(String id) {
@@ -38,5 +55,43 @@ class InFileRepository implements TaskRepository {
             }
         }
         throw new IllegalArgumentException("the task with " + id + "does not exist");
+    }
+
+    public void save() throws IOException {
+        String arrayToJson = objectMapper.writeValueAsString(this.db);
+        this.saveFile(arrayToJson);
+    }
+
+    public void init() throws IOException {
+        String data = String.valueOf(this.readFile());
+        this.db = objectMapper.readValue(data, mapType);
+    }
+
+    private StringBuilder readFile() {
+        StringBuilder data = new StringBuilder("");
+        try {
+            File myObj = new File(jsonFilePath);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                data.append(myReader.nextLine());
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    private void saveFile(String json){
+        try {
+            FileWriter myWriter = new FileWriter(jsonFilePath);
+            myWriter.write(json);
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
