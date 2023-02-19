0ed3120db84285eae28afdd53eac02a0d74fcefa
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2023-02-19 22:45:33 +0100

    :white_check_mark: ParserValidatorTest

---
+++ src/test/java/domain/services/InputParserTest.java
@@ -0,0 +1,33 @@
+package domain.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputParserTest {
+
+    @Test
+    void instructionValidation() {
+        assertTrue(true);
+    }
+
+    @Test
+    void addInstruction() {
+        assertTrue(true);
+    }
+
+    @Test
+    void updateInstruction() {
+        assertTrue(true);
+    }
+
+    @Test
+    void listInstruction() {
+        assertTrue(true);
+    }
+
+    @Test
+    void removeInstruction() {
+        assertTrue(true);
+    }
+}
